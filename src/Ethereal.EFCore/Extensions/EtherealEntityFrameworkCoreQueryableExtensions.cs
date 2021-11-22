302be365507a3e56a4fc2241454aeae6dca8415f
HUMAOSHUAI <hu15150@126.com>
2021-11-22 18:02:35 +0800

    cl

--- src/Ethereal.EFCore/Extensions/EtherealEntityFrameworkCoreQueryableExtensions.cs
+++ src/Ethereal.EFCore/Extensions/EtherealEntityFrameworkCoreQueryableExtensions.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore.Query.Internal;
 using System;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
